126486bab7ca4505020482c5ff895a0177557f93
nickrim <anisim@gmail.com>
2013-11-10 07:55:04 -0800

    commit hw_19

---
+++ ITA/hw_19/run_script_19_01.bat
@@ -0,0 +1,5 @@
+:: Commands to execute script script_19_01.rb
+
+ ruby ./lib/script_19_01.rb --domain www.shopping.com > ./report/shopping.txt
+ ruby ./lib/script_19_01.rb --domain www.alegro.com > ./report/alegro.txt
+ ruby ./lib/script_19_01.rb --domain www.epinions.com > ./report/epinions.txt
---
+++ ITA/hw_19/run_script_19_01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Commands to execute script script_19_01.rb
+
+ruby ./lib/script_19_01.rb --domain www.shopping.com > ./report/shopping.txt
+ruby ./lib/script_19_01.rb --domain www.alegro.com > ./report/alegro.txt
+ruby ./lib/script_19_01.rb --domain www.epinions.com > ./report/epinions.txt
