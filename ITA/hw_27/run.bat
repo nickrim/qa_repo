9ebfc48522f410a1c0f56c3f97e05f80a9727e0b
nickrim <anisim@gmail.com>
2013-11-20 12:39:48 -0800

    hw_27

---
+++ ITA/hw_27/run.bat
@@ -0,0 +1,6 @@
+:: Command to execute the test
+
+:: call bundle exec ruby ./lib/static_element_validation.rb -d http://www.khatilov.com -o ./reports/v1_report.txt -p ./etc/test_specs/sign_up_01.json
+call bundle exec ruby ./lib/static_element_validation.rb -d http://www.khatilov.com -o ./reports/v4_report.txt -p ./etc/test_specs/sign_up_04.json
+
+ pause
