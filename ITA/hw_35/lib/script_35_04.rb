f5155c53ddd74e3151685b271c2ac5b67294773f
nickrim <anisim@gmail.com>
2013-12-08 09:10:01 -0800

    hw_35

---
+++ ITA/hw_35/lib/script_35_04.rb
@@ -0,0 +1,44 @@
+require 'optparse'
+require 'json'
+
+str =  __FILE__
+reg = /\/[\w\.]+$/
+v = str.match reg
+key =  v.to_s.reverse.chop.reverse
+
+items_array = []
+
+OptionParser.new do |opts|
+	opts.on("-i", "--input") {$input_file = ARGV[0]}
+	opts.on("-o", "--output") {$output_file = ARGV[0]}
+end.parse!
+
+json = File.read($input_file)
+var = JSON.parse(json)
+
+value =  var[key]
+
+items_array = []
+
+value.each do |a, b|
+items_array.push b
+end
+
+ip=""
+
+items_array.each {|item| 
+ip += item.chomp
+ip += "."
+}
+
+ip = ip.chop
+
+
+report = File.open($output_file, "w")
+report << "<html><head><title>HW # 34</title></head><body bgcolor='#D4D0C8' leftmargin='0' rightmargin='0' topmargin='0' bottommargin='0' marginheight='0' marginwidth='0'>
+<table bgcolor='#FFFFFF' width='100%' border='1' cellpadding='0' cellspacing='0' style='border-collapse: collapse' bordercolor='#333333'>
+<tr style='font-family:Verdana, Tahoma; color:0048B0; font-size:12pt;'><td>"
+
+report << "#{key}: My IP address is #{ip}"
+
+report << "</td></tr></table></body></html>"
---
+++ ITA/hw_35/run_35.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Commands to execute script run_35.rb
+
+# ruby ./lib/script_35_01.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_02.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_03.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_04.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_05.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_06_A.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_06_B.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_07_A.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_07_B.rb -i ./etc/input_35.json -o ./reports/index.html 
+# ruby ./lib/script_35_08.rb -i ./etc/input_35.json -o ./reports/index.html 
